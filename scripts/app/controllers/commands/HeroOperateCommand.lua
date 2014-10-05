70be0dab9e2fde17c6c95e7e985ae8821b7dc575
wangshao <exwz@163.com>
2014-10-06 00:53:39 +0800

    Revert "test merge"
    This reverts commit 0001a7ae56b0ab63a897b656764470a73b17dce2.

--- scripts/app/controllers/commands/HeroOperateCommand.lua
+++ scripts/app/controllers/commands/HeroOperateCommand.lua
@@ -15,11 +15,6 @@ end
 function HeroOperateCommand:execute()
 
     HeroOperateCommand.super:execute(self)
-
-
-    local a = 1
-
-
     local object=self.map_:getObject(self.opObjId_)
     if object ~= nil then
         if not object:GetModel():isDead() then
